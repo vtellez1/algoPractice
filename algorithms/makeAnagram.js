8935e49797324903d80c199465be70e20c633630
Vanessa <vanessatellez45@gmail.com>
2020-07-23 18:06:25 -0400

    added instructors solution

--- algorithms/makeAnagram.js
+++ algorithms/makeAnagram.js
@@ -23,3 +23,33 @@ function validAnagram(str1, str2){
   
     return isAnagram;
   }
+
+  validAnagram('car', 'rac');
+
+// Instructor's - (Colt Steele's) solution:
+  function validAnagram(first, second){
+    let lookup = {};
+  
+    if (first.length !== second.length){
+      return false;
+    }
+   
+    for(let i = 0; i < first.length; i++){
+      let letter = first[i]
+      // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+  
+    for (let i = 0; i < second.length; i++){
+      let letter = second[i];
+  
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]){
+          return false;
+      } else {
+          lookup[letter] -= 1;
+      }
+    }
+
+    return true;
+  }
